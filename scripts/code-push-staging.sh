a00bd239c7e4440428123e7e6da69caa9bea45df
Brandon Yu <me@brandonyu.com>
2018-02-13 23:34:45 -0800

    test

--- scripts/code-push-staging.sh
+++ scripts/code-push-staging.sh
@@ -1,2 +1,3 @@
-export npm_package_version=`npm run npm_package_version | tail -1`
-code-push release byunique/tapnumber-ios ./release $npm_package_version --noDuplicateReleaseError
+#export npm_package_version=`npm run npm_package_version | tail -1`
+#code-push release byunique/tapnumber-ios ./release $npm_package_version --noDuplicateReleaseError
+code-push release byunique/tapnumber-ios --noDuplicateReleaseError
